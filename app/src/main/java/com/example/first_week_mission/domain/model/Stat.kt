89855fd3cef3a7a0a66057e3398e94dd02ca49f2
KimKwangYoung <kwangyoung.dev@gmail.com>
2024-06-04 14:54:16 +0900

    chore: git cache remove

--- app/src/main/java/com/example/first_week_mission/domain/model/Stat.kt
+++
@@ -1,6 +0,0 @@
-package com.example.first_week_mission.domain.model
-
-data class Stat(
-    val baseStat: Int,
-    val name: String
-)
--- app/src/main/java/com/example/first_week_mission/model/FlavorText.kt
+++
@@ -1,8 +0,0 @@
-package com.example.first_week_mission.model
-
-import com.google.gson.annotations.SerializedName
-
-data class FlavorText(
-    @SerializedName("flavor_text") val flavorText: String,
-    val language: Info
-)
